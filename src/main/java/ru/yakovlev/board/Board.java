c0e31ed32c03f78ae5d3649741cb49dbd2f5a4f0
Alexander Yakovlev <sanyakovlev@yandex.ru>
2019-08-11 17:43:06 +0700

    Определен интерфейс игрового.

---
+++ src/main/java/ru/yakovlev/board/Board.java
@@ -0,0 +1,29 @@
+package ru.yakovlev.board;
+
+import ru.yakovlev.board.cells.Cell;
+import ru.yakovlev.board.cells.CellType;
+import ru.yakovlev.board.events.EventList;
+
+/**
+ * Интерфейс игрового поля.
+ *
+ * @since 0.1
+ */
+public interface Board extends BoardPropertiesProvider {
+
+    Cell cell(BoardCoordinate coordinate);
+
+    /**
+     * Устанавливает новое значение типа ячейки по переданным координатам.
+     * @param coordinate координата ячейки.
+     * @param type новый тип ячейки.
+     */
+    void setType(BoardCoordinate coordinate, CellType type);
+
+    /**
+     * Оповещение игрового поля, что было открыто переданное количество ячеек.
+     * @param amount количество открытых ячеек на игрвом поле.
+     * @return список событий возникших из-за данного действия.
+     */
+    EventList cellsWasOpened(int amount);
+}
