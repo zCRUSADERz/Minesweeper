179ac522d5242bba7b57178915d15812bb99b3b7
Alexander Yakovlev <sanyakovlev@yandex.ru>
2019-08-12 10:52:34 +0700

    Реализовано Images.

---
+++ src/main/java/ru/yakovlev/Images.java
@@ -0,0 +1,30 @@
+package ru.yakovlev;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Ищет необходимые иконки в classpath.
+ *
+ * @since 0.1
+ */
+public class Images {
+    private Map<String, ImageIcon> images = new HashMap<>();
+
+    public final ImageIcon image(final String imageName) {
+        return this.images.computeIfAbsent(
+            imageName,
+            new Function<String, ImageIcon>() {
+                @Override
+                public ImageIcon apply(final String s) {
+                    System.out.println(s);
+                    return new ImageIcon(
+                        getClass().getResource(String.format("/img/%s.png", s))
+                    );
+                }
+            }
+        );
+    }
+}
