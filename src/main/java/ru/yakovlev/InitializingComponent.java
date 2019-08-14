40acff17ac656fa3be29e7a2ecd652a8f1acf322
Alexander Yakovlev <sanyakovlev@yandex.ru>
2019-08-14 10:16:54 +0700

    Определен интерфейс компонента нуждающегося в инициализации.

---
+++ src/main/java/ru/yakovlev/InitializingComponent.java
@@ -0,0 +1,11 @@
+package ru.yakovlev;
+
+/**
+ * Компонент приложения, нуждающийся в инициализации перед его использованием.
+ *
+ * @since 0.1
+ */
+public interface InitializingComponent {
+
+    void init();
+}
